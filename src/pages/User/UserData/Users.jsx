710e31a28d1335045249f5842d303c83335a9d0d
Rijvan Juneja <rijvan.juneja@esparkbizmail.com>
2024-07-18 10:48:14 +0530

    add a chart in user Dashboard

--- src/pages/User/UserData/Users.jsx
+++ src/pages/User/UserData/Users.jsx
@@ -1,6 +1,9 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import DynamicTable from "../../../common/components/Tables/DynamicTable";
 import useFetch from "../../../hooks/useFetch";
+import { format } from "date-fns";
+import Loader from "../../../common/components/Layout/Loader";
+
 
 
 const Users = () => {
@@ -14,7 +17,12 @@ const Users = () => {
 
       try {
         const response = await sendData(`/users/api/userlist`);
-        setUsers(response.data)
+        const formattedUsers = response.data.map(user => ({
+          ...user,
+          role_name: user.role.role_name,
+          created_at: format(new Date(user.created_at), 'yyyy-MM-dd HH:mm:ss'),
+        }));
+        setUsers(formattedUsers)
         
       } catch (error) {
         console.log(error);
@@ -23,24 +31,24 @@ const Users = () => {
     )()
   },[])
 
-
   const columns = [
     { Header: 'User Id', accessor: 'id' },
     { Header: 'First Name', accessor: 'first_name' },
     { Header: 'Last Name', accessor: 'last_name' },
     { Header: 'Email', accessor: 'email' },
     { Header: 'Mobile Number', accessor: 'mobile_number' },
-    { Header: 'Role', accessor: 'role.role_name' },
+    { Header: 'Role', accessor: 'role_name', },
     { Header: 'Registation Date', accessor: 'created_at' },
   ];
 
   return (
     <>
+      {loading && <Loader />}
       <h3 className="mx-9 mt-9 text-3xl font-bold dark:text-white">User List</h3>
 
       <div className="mx-9 mt-7 bg-white p-4">
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg p-5" >
-          <DynamicTable columns={columns} data={[]} />
+          <DynamicTable columns={columns} data={users} />
         </div>
       </div>
     </>
